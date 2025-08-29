63c489d9ca180362e8c6155e063d9413a6e72da9
gamerquantannymars <oshiieyov@hotmail.com>
2025-08-29 06:37:12 +0300

    Add detailed comments for configuration settings

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Cấu hình thêm có thể được thêm vào đây trong tương lai
     # Các thiết lập khác có thể được thêm vào nếu cần thiết
+    # Ví dụ: 'DEBUG' có thể được thêm cho môi trường phát triển
+    DEBUG = os.getenv('DEBUG', 'False') == 'True'
