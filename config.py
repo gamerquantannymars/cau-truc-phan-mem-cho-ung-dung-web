5213b0ac693466c00bfb45282f02e80abf3adf21
gamerquantannymars <oshiieyov@hotmail.com>
2025-08-29 06:24:25 +0300

    Add comments to config class for better clarity

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    # Database URI configuration
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'sqlite:///site.db'
+    # Disable track modifications to save resources
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
