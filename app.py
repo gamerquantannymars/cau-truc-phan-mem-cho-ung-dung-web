b53d26fdfb1005adcabbdfc702ae0ff45480e5be
gamerquantannymars <oshiieyov@hotmail.com>
2025-08-29 06:34:17 +0300

    Add comment to clarify the purpose of the Flask app

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ def home():
     # Render the homepage using the 'index.html' template
     return render_template('index.html')
 
+# Start the Flask web server when the script is executed directly
 if __name__ == '__main__':
     app.run(debug=True)
