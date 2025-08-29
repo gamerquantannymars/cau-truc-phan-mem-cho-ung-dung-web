31d023273fbad123aa62d2e7facd6a622c52d0c5
gamerquantannymars <oshiieyov@hotmail.com>
2025-08-29 06:29:20 +0300

    Add comments to clarify route functionality

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # Render the homepage using the 'index.html' template
     return render_template('index.html')
 
 if __name__ == '__main__':
